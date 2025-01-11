660007e0b8f3381473aa4cc9fd822959621956b9
Riley Williams <ririwilliams.rw@gmail.com>
2025-01-11 16:04:22 -0600

    I was overthinking it for sure. If both of them are sorted it means each char is in place already, so no need to compare them more than a simpled equality check between the two strings

--- solutions/valid-anagram.py
+++ solutions/valid-anagram.py
@@ -3,15 +3,4 @@ class Solution:
         sortedS = sorted(s)
         sortedT = sorted(t)
 
-        count = 0
-        for i in range(0, len(sortedS)):
-            if sortedS[i] == sortedT[i]:
-                count += 1
-                print(count)
-            
-            
-        if count == len(sortedS) and count == len(sortedT):
-                return True
-        return False
-    
-    print(isAnagram("a", "ab"))
+        return sortedS == sortedT
