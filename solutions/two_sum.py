3f0607617881af333fc698f970cf3b9a2d324366
Riley Williams <ririwilliams.rw@gmail.com>
2025-01-13 17:26:49 -0600

    Using hash maps here, and since the problem states that there is a solution in every list we can do a one pass solution which means we are adding to the hash map as we go instead of having to first add the entire list to the hash map and then iterating over it see if difference exists

--- solutions/two_sum.py
+++ solutions/two_sum.py
@@ -1,8 +1,19 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if j == i:
-                    break
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+    def twoSum(nums: list[int], target: int) -> list[int]:
+        numMap = {} # val -> index
+
+        for i, n in enumerate(nums):
+            difference = target - n
+            
+
+            if difference in numMap:
+                return [numMap[difference], i]
+
+            numMap[n] = i
+
+        #for i in range(len(nums)):
+            #for j in range(len(nums)):
+                #if j == i:
+                    #break
+                #if nums[i] + nums[j] == target:
+                    #return [i,j]
