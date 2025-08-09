b7245cb86578bd2d5db737945c5a613713c5534d
ririthewizard <ririwilliams.rw@gmail.com>
2025-08-09 00:30:10 -0500

    Stack makes a bit of sense

--- solutions/java/validParens.java
+++ solutions/java/validParens.java
@@ -1,5 +1,22 @@
 class Solution {
 	public boolean isValid(String s) {
+		Deque<Character> stack = new ArrayDeque<>();
+		java.util.Map<Character, Character> closeToOpen = new java.util.HashMap<>();
+		closeToOpen.put(')', '(');
+		closeToOpen.put('}', '{');
+		closeToOpen.put(']', '[');
 
+		for (char c : s.toCharArray()) {
+			if (closeToOpen.containsKey(c)) {
+				if (!stack.isEmpty() && stack.peek() == closeToOpen.get(c)) {
+					stack.pop();
+				} else {
+					return false;
+				}
+			} else {
+				stack.push(c);
+			}
+		}
+		return stack.isEmpty();
 	}
 }
