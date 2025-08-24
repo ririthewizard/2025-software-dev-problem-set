7d47f15f19dfa9924dbf30d3fddb55e8c8354779
ririthewizard <ririwilliams.rw@gmail.com>
2025-08-23 22:43:06 -0500

    Love Binary Search

---
+++ solutions/java/binarySearch.java
@@ -0,0 +1,16 @@
+public int search(int[] nums, int target) {
+	int left = 0, right = nums.length - 1;
+
+	while (left <= right) {
+		int middle = left + ((right - left) / 2);
+
+		if (target < nums[middle]) {
+			right = middle - 1;
+		} else if (target > nums[middle]) {
+			left = middle + 1;
+		} else {
+			return middle;
+		}
+	}
+	return -1;
+}
