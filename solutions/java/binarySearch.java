7ba764f4478a62232d3008a07be3c856507d2773
ririthewizard <ririwilliams.rw@gmail.com>
2025-08-25 20:59:20 -0500

    Cool! I feel fairly confident about Binary Search provided I revisit every so often

--- solutions/java/binarySearch.java
+++ solutions/java/binarySearch.java
@@ -3,14 +3,14 @@ public int search(int[] nums, int target) {
 
 	while (left <= right) {
 		int middle = left + ((right - left) / 2);
-
-		if (target < nums[middle]) {
-			right = middle - 1;
-		} else if (target > nums[middle]) {
-			left = middle + 1;
-		} else {
+		if (target == nums[middle]) {
 			return middle;
+		} else if (target < nums[middle]) {
+			right = middle - 1;
+		} else {
+			left = middle + 1;
 		}
 	}
+
 	return -1;
 }
