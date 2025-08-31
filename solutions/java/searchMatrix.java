e2df814d866a57a072cce13db445d4a9e349d2a6
ririthewizard <ririwilliams.rw@gmail.com>
2025-08-31 14:36:32 -0500

    Search Matrix

--- solutions/java/searchMatrix.java
+++ solutions/java/searchMatrix.java
@@ -1,4 +1,5 @@
 public boolean searchMatrix(int[][] matrix, int target) {
+<<<<<<< Updated upstream
     int ROWS = matrix.length;
     int COLS = matrix[0].length;
 
@@ -33,4 +34,39 @@ public boolean searchMatrix(int[][] matrix, int target) {
     }
 
     return false;
+=======
+	int ROWS = matrix.length;
+	int COLS = matrix[0].length;
+
+	int top = 0, bot = ROWS - 1;
+	while (top <= bot) {
+		int row = top + ((bot - top) / 2);
+		if (target > matrix[row][COLS - 1]) {
+			top = row + 1;
+		} else if (target < matrix[row][0]) {
+			bot = row - 1;
+		} else {
+			break;
+		}
+	}
+
+	if (!(top <= bot)) {
+		return false;
+	}
+
+	int row = top + ((bot - top) / 2);
+	int left = 0, right = COLS - 1;
+	while (left <= right) {
+		int middle = left + ((right - left) / 2);
+		if (target > matrix[row][middle]) {
+			left = middle + 1;
+		} else if (target < matrix[row][middle]) {
+			right = middle - 1;
+		} else {
+			return true;
+		}
+	}
+
+	return false;
+>>>>>>> Stashed changes
 }
