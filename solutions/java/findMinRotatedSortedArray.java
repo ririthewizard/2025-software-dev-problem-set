fe3d6b05fcfe9174b678a50db91e8145d445cba2
ririthewizard <ririwilliams.rw@gmail.com>
2025-09-01 23:44:17 -0500

    More BS!

---
+++ solutions/java/findMinRotatedSortedArray.java
@@ -0,0 +1,22 @@
+public int findmin(int[] nums) {
+    int left = 0;
+    int right = nums.length - 1;
+    int res = nums[0];
+
+    while (left <= right) {
+	if (nums[left] < nums[right]) {
+	    res = Math.min(res, nums[left]);
+	    break;
+	}
+
+	int middle = left + ((right - left) / 2);
+	res = Math.min(res, nums[middle]);
+
+	if (nums[middle] >= nums[left]) {
+	    left = middle + 1;
+	} else {
+	    right = middle - 1;
+	}
+    }
+    return res;
+}
